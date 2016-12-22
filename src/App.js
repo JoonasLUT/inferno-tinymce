fc5fc8df350a8968ab5f0b91ac61d61b3adf0507
Joonas Salminen <joonas.salminen@visma.com>
2016-12-22 13:46:47 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import Inferno from 'inferno';
+import Component from 'inferno-component';
+import {TinyMCE} from './lib/components/TinyMCE';
+import Logo from './logo';
+import './App.css';
+
+class App extends Component {
+
+    handleEditorChange = (e) => {
+        console.log('Content was updated:', e.target.getContent());
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <Logo width="80" height="80"/>
+                    <h2>Welcome to Inferno</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <div>
+                    <TinyMCE
+                        content="<p>This is the initial content of the editor</p>"
+                        config={{
+                            plugins: 'link image code',
+                            toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
+                        }}
+                        onChange={this.handleEditorChange}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/lib/helpers/uuid.js
@@ -0,0 +1,4 @@
+let count = 0;
+module.exports = function uuid() {
+  return 'inferno-tinymce-' + count++;
+};
